11a47a58def76e5bbbf4b696445c71a1d942f741
Inozemcev-INS-19 <71772161+Inozemcev-INS-19@users.noreply.github.com>
2020-09-28 12:47:56 +0300

    Add files via upload

---
+++ Laba6/par.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba_6
+{
+    class par
+    {
+        public double r;
+
+        public double GetV() { return Math.PI * Math.Pow(r, 3) * 4/3; }
+
+        public double GetS() { return 4 * Math.PI * Math.Pow(r, 2); }
+
+        public void Info()
+        {
+            string str =
+                "      ***********      \n" +
+                "    **            **   \n" +
+                "   **      Ш       **  \n" +
+                "  **       А        ** \n" +
+                "  **       Р        ** \n" +
+                "   **              **  \n" +
+                "    **            **   \n" +
+                "      ***********      \n";
+
+            Console.WriteLine(str);
+            Console.WriteLine(string.Format("R = {0:0.00}, ", r));
+            Console.WriteLine(string.Format("V = {0:0.00}", GetV()));
+            Console.WriteLine(string.Format("S = {0:0.00}", GetS()));
+        }
+
+        public void Load()
+        {
+            r = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
