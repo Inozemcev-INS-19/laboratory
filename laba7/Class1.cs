fdf3ae94b133dc7c19c1ef8461b6be8d7262396d
Inozemcev-INS-19 <71772161+Inozemcev-INS-19@users.noreply.github.com>
2020-10-05 23:01:25 +0300

    Add files via upload

---
+++ laba7/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7laba
+{
+    class Class1
+    {
+        private double d1, d2;
+        //Конструктор
+        public Class1() { }
+        //Конструктор
+        public Class1(double pd1, double pd2)
+        {
+            d1 = pd1;
+            d2 = pd2;
+        }
+        //Конструктор
+        public static Class1 Var2()
+        {
+            string pdp = "\nЭто ромб, который выводится с помощью конструктора";
+            Console.WriteLine(pdp);
+            double dd1 = Convert.ToDouble(Console.ReadLine());
+            double dd2 = Convert.ToDouble(Console.ReadLine());
+            return new Class1(dd1, dd2);
+        }
+
+        public void Load()
+        {
+            d1 = Convert.ToDouble(Console.ReadLine());
+            d2 = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double GetS() { return d1 * d2 * 1 / 2; }
+
+        public double GetP() { return Math.Sqrt(Math.Pow(d1, 2) + Math.Pow(d2, 2)) * 2; }
+
+        //Метод
+        public void Info()
+        {
+            string str =
+                "          ****         \n" +
+                "         **  **        \n" +
+                "        **    **       \n" +
+                "       **      **      \n" +
+                "      **        **     \n" +
+                "     **          **    \n" +
+                "    **            **   \n" +
+                "   **      Р       **  \n" +
+                "  **       О        ** \n" +
+                "  **       М        ** \n" +
+                "   **      Б       **  \n" +
+                "    **            **   \n" +
+                "     **          **    \n" +
+                "      **        **     \n" +
+                "       **      **      \n" +
+                "        **    **       \n" +
+                "         **  **        \n" +
+                "          ****         \n";
+            Console.WriteLine(str);
+            Console.WriteLine(string.Format("D1 = {0:0.00}, ", d1));
+            Console.WriteLine(string.Format("D2 = {0:0.00}, ", d2));
+            Console.WriteLine(string.Format("S = {0:0.00}", GetS()));
+            Console.WriteLine(string.Format("P = {0:0.00}", GetP()));
+        }
+        //Метод
+        public void Info(ConsoleColor fg, ConsoleColor bgs)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bgs;
+            Console.Clear();
+            Info();
+        }
+
+        
+
+
+    }
+}
---
+++ laba7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+namespace _7laba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"Class1_output.txt");
+            var new_in = new StreamReader(@"Class1_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Class1 p1, p2;
+#if DEBUG
+            p2= new Class1(10, 12);
+            p2.Info(ConsoleColor.Red, ConsoleColor.DarkBlue);
+#endif
+
+#if !DEBUG
+
+            p1 = new Class1(4.5, 10.2);
+            p1.Info(ConsoleColor.Red, ConsoleColor.DarkBlue);
+            p2 = Class1.Var2();
+            p2.Info();
+            
+
+#endif
+
+
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#endif
+
+#if DEBUG
+            Console.ReadKey();
+#endif
+        }
+    }
+}
