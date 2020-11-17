1199f665467867500f37fd75b52b213d04d37345
Inozemcev-INS-19 <71772161+Inozemcev-INS-19@users.noreply.github.com>
2020-11-17 20:35:47 +0300

    Add files via upload

---
+++ laba10/Matr.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba10
+{
+    class Matr
+    {
+        private float[,] mas;
+        int n, m;
+        //Генерация
+        public void Generator(int N, int M)
+        {
+            n = N;
+            m = M;
+            Random ran = new Random(DateTime.Now.Millisecond);
+
+            mas = new float[N, M];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                    mas[i, j] = (float)ran.Next(-1000, 1000);
+            }
+        }
+        //Сохранение
+        public void Save(string pFileName) {
+            if (mas.Length > 0)
+            {
+                if (File.Exists(pFileName))
+                    File.Delete(pFileName);
+
+                FileInfo f = new FileInfo(pFileName);
+                TextWriter tw = f.CreateText();
+
+                tw.WriteLine(m.ToString());
+                tw.WriteLine(n.ToString());
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++)
+                        tw.WriteLine(i.ToString() + " " + j.ToString() + " " + mas[i, j]);
+                    
+                }
+                tw.Close();
+            }
+        }
+
+        public Boolean LoadMatrix(string pFileName) {
+            if (File.Exists(pFileName))
+            {
+                try
+                {
+                    TextReader tr = File.OpenText(pFileName);
+                    n = Convert.ToInt32(tr.ReadLine());
+                    m = Convert.ToInt32(tr.ReadLine());
+
+                    mas = new float[n, m];
+
+                    string line;
+                    string[] substring;
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < m; j++)
+                        {
+                            line = tr.ReadLine();
+                            substring = line.Split(new char[] { ' ' }, 3);
+                            mas[i, j] = Convert.ToSingle(substring[2]);
+
+                        }
+                    }
+                    tr.Close();
+                    return true;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }                       
+            }
+            return false;     
+        }
+
+        public void PrintMatrix() {
+            if (mas.Length > 0) {
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++)
+                        Console.Write(mas[i, j]+ " ");
+
+                    Console.WriteLine();
+                }       
+            }
+        }
+
+        float sum = 0;
+        public float SummaElementov() { 
+            if(mas.Length > 0)
+            {
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) 
+                        sum += mas[i, j];
+                }
+                
+            }
+            
+            return sum;
+        }
+
+        float razn = 0;
+        public float RaznElementov()
+        {
+            if (mas.Length > 0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                        sum += mas[i, j];
+                }
+
+            }
+
+            return sum;
+        }
+
+
+        public void otvet(ref float sum) {    
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ laba10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba10
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Matr x = new Matr();
+            x.Generator(2, 2);
+            x.Save("Matrix1.txt");
+
+            Matr y = new Matr();
+            y.Generator(2, 2);
+            y.Save("Matrix2.txt");
+
+            Console.WriteLine("Матрица 1");
+            if (x.LoadMatrix("Matrix1.txt"))
+                x.PrintMatrix();
+
+            Console.WriteLine();
+            Console.WriteLine("Матрица 2");
+            if (y.LoadMatrix("Matrix2.txt"))
+                y.PrintMatrix();
+
+            Console.WriteLine();
+            Console.WriteLine("Сумма");
+            if (x.LoadMatrix("Matrix1.txt") && y.LoadMatrix("Matrix2.txt")) {
+                Console.WriteLine(x.SummaElementov() - y.SummaElementov());
+            }
+        }
+    }
+}
+
