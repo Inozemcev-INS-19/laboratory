039bb2fc49733050456acc3b4cbff64614fafbfb
Inozemcev-INS-19 <71772161+Inozemcev-INS-19@users.noreply.github.com>
2020-12-17 17:43:29 +0300

    Add files via upload

---
+++ laba11/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba_11
+{
+    enum CategoryType
+    {
+        A, B, C, D
+    }
+
+    class Person
+    {
+        public String ID { get; set; }
+        public String Name { get; set; }
+        public String Company { get; set; }
+        public Double Price { get; set; }
+        public int Count { get; set; }
+        public CategoryType Category { get; set; }
+        public int Discount { get; set; }
+
+
+        public static Person Create(String str)
+        {
+            Person p = new Person();
+            string[] e = str.Split(',');
+            p.ID = e[0].Trim();
+            p.Name = e[1].Trim();
+
+            p.Company = e[2].Trim();
+            p.Price = Convert.ToDouble(e[3].TrimStart('$').Replace('.', ','));
+            p.Count = Convert.ToInt32(e[4].Trim());
+
+            String categ = e[5].Trim();
+            if (categ == "A") p.Category = CategoryType.A;
+            else if (categ == "B") p.Category = CategoryType.B;
+            else if (categ == "C") p.Category = CategoryType.C;
+            else p.Category = CategoryType.D;
+
+            p.Discount = Convert.ToInt32(e[6].Trim());
+
+
+            return p;
+        }
+
+        public override string ToString()
+        {
+            String s = string.Format("\n" +
+                "ID: {0}, Name: {1},\n" +
+                "Company: {2}, Price: {3}, Count: {4},\n" +
+                "Category: {5}, Discount: {6}", ID, Name, Company, Price, Count, Category, Discount);
+            return s;
+        }
+    }
+}
---
+++ laba11/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace laba_11
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            StreamReader fileIn = new StreamReader(
+                "lr11_31.csv");
+
+
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            var new_out = new StreamWriter(@"output.txt");
+            Console.SetOut(new_out);
+#endif
+
+            List<Person> all = new List<Person>();
+
+            try
+            {
+                String line = fileIn.ReadLine();
+                while ((line = fileIn.ReadLine()) != null)
+                {
+                    all.Add(Person.Create(line));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Всего пользователей: {0}", all.Count);
+            
+
+            Console.WriteLine();
+            Console.WriteLine("**** 1 Task ****");
+            float aType = all.FindAll(p => p.Category == CategoryType.A).ToList().Count;
+            float bType = all.FindAll(p => p.Category == CategoryType.B).ToList().Count;
+            float cType = all.FindAll(p => p.Category == CategoryType.C).ToList().Count;
+
+            Console.WriteLine("Товаров категории А: {0}", aType);
+            Console.WriteLine("Товаров категории B: {0}", bType);
+            Console.WriteLine("Товаров категории C: {0}", cType);
+            Console.WriteLine("Товаров категории D: {0}", all.Count-(aType+bType+cType));
+
+            Console.WriteLine();
+            Console.WriteLine("**** 2 Task ****");
+
+            double Volume = (from p in all
+                             where (p.Company == "")
+                             select p.Price).Sum();
+            Console.WriteLine("Суммарный объем (в валюте) без поставщика: {0:0.000}", Volume);
+            
+            Console.WriteLine();
+            Console.WriteLine("**** 3 Task ****");
+            
+            double  MaxCountA = (from p in all 
+                                where(p.Category == CategoryType.A) 
+                                select p.Price).Max();
+            Console.WriteLine("Cамый дорогой товар категории A: {0}", MaxCountA);
+            double MaxCountB = (from p in all
+                                where (p.Category == CategoryType.B)
+                                select p.Price).Max();
+            Console.WriteLine("Cамый дорогой товар категории B: {0}", MaxCountB);
+            double MaxCountC = (from p in all
+                                where (p.Category == CategoryType.C)
+                                select p.Price).Max();
+            Console.WriteLine("Cамый дорогой товар категории C: {0}", MaxCountC);
+            double MaxCountD = (from p in all
+                                where (p.Category == CategoryType.D)
+                                select p.Price).Max();
+            Console.WriteLine("Cамый дорогой товар категории D: {0}", MaxCountD);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("**** 4 Task ****");
+            int MaxDiscount = (from p in all select p.Discount).Max(); 
+            Person MaxTovar = (from p in all where p.Discount == MaxDiscount select p).First();
+            Console.WriteLine("Товар с максимальной скидкой: {0}", MaxTovar);
+
+#if !DEBUG
+            Console.SetOut(save_out);
+            new_out.Close();
+#else
+            Console.ReadKey();
+#endif
+
+        }
+    }
+}
