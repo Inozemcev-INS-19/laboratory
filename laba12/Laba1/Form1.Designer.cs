2da5d7b1829834b6bbebace3838e2a4453c22d56
Inozemcev-INS-19 <71772161+Inozemcev-INS-19@users.noreply.github.com>
2021-04-13 21:25:12 +0300

    Add files via upload

---
+++ laba12/Laba1/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxG = new System.Windows.Forms.TextBox();
+            this.textBoxE = new System.Windows.Forms.TextBox();
+            this.e = new System.Windows.Forms.Label();
+            this.g = new System.Windows.Forms.Label();
+            this.textBoxW = new System.Windows.Forms.TextBox();
+            this.textBoxQ = new System.Windows.Forms.TextBox();
+            this.w = new System.Windows.Forms.Label();
+            this.q = new System.Windows.Forms.Label();
+            this.buttonСalculate = new System.Windows.Forms.Button();
+            this.Answer = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.x = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxG
+            // 
+            this.textBoxG.Location = new System.Drawing.Point(139, 74);
+            this.textBoxG.Name = "textBoxG";
+            this.textBoxG.Size = new System.Drawing.Size(100, 20);
+            this.textBoxG.TabIndex = 0;
+            this.textBoxG.Text = "0";
+            // 
+            // textBoxE
+            // 
+            this.textBoxE.Location = new System.Drawing.Point(139, 117);
+            this.textBoxE.Name = "textBoxE";
+            this.textBoxE.ReadOnly = true;
+            this.textBoxE.Size = new System.Drawing.Size(100, 20);
+            this.textBoxE.TabIndex = 1;
+            this.textBoxE.Text = "0";
+            // 
+            // e
+            // 
+            this.e.AutoSize = true;
+            this.e.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.e.Location = new System.Drawing.Point(98, 121);
+            this.e.Name = "e";
+            this.e.Size = new System.Drawing.Size(16, 16);
+            this.e.TabIndex = 2;
+            this.e.Text = "e";
+            // 
+            // g
+            // 
+            this.g.AutoSize = true;
+            this.g.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.g.Location = new System.Drawing.Point(98, 78);
+            this.g.Name = "g";
+            this.g.Size = new System.Drawing.Size(16, 16);
+            this.g.TabIndex = 3;
+            this.g.Text = "g";
+            // 
+            // textBoxW
+            // 
+            this.textBoxW.Location = new System.Drawing.Point(139, 167);
+            this.textBoxW.Name = "textBoxW";
+            this.textBoxW.ReadOnly = true;
+            this.textBoxW.Size = new System.Drawing.Size(100, 20);
+            this.textBoxW.TabIndex = 12;
+            this.textBoxW.Text = "0";
+            // 
+            // textBoxQ
+            // 
+            this.textBoxQ.Location = new System.Drawing.Point(139, 213);
+            this.textBoxQ.Name = "textBoxQ";
+            this.textBoxQ.Size = new System.Drawing.Size(100, 20);
+            this.textBoxQ.TabIndex = 11;
+            this.textBoxQ.Text = "0";
+            // 
+            // w
+            // 
+            this.w.AutoSize = true;
+            this.w.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.w.Location = new System.Drawing.Point(97, 167);
+            this.w.Name = "w";
+            this.w.Size = new System.Drawing.Size(17, 16);
+            this.w.TabIndex = 6;
+            this.w.Text = "w";
+            // 
+            // q
+            // 
+            this.q.AutoSize = true;
+            this.q.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.q.Location = new System.Drawing.Point(98, 217);
+            this.q.Name = "q";
+            this.q.Size = new System.Drawing.Size(16, 16);
+            this.q.TabIndex = 7;
+            this.q.Text = "q";
+            // 
+            // buttonСalculate
+            // 
+            this.buttonСalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonСalculate.Location = new System.Drawing.Point(139, 251);
+            this.buttonСalculate.Name = "buttonСalculate";
+            this.buttonСalculate.Size = new System.Drawing.Size(216, 79);
+            this.buttonСalculate.TabIndex = 8;
+            this.buttonСalculate.Text = "Сalculate";
+            this.buttonСalculate.UseVisualStyleBackColor = true;
+            this.buttonСalculate.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonСalculate_MouseClick);
+            // 
+            // Answer
+            // 
+            this.Answer.AutoSize = true;
+            this.Answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Answer.Location = new System.Drawing.Point(386, 144);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(0, 20);
+            this.Answer.TabIndex = 10;
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(362, 74);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 13;
+            this.textBoxX.Text = "0";
+            // 
+            // x
+            // 
+            this.x.AutoSize = true;
+            this.x.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x.Location = new System.Drawing.Point(335, 69);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(20, 24);
+            this.x.TabIndex = 14;
+            this.x.Text = "x";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Laba1.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(274, 137);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(316, 50);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 379);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.x);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.Answer);
+            this.Controls.Add(this.buttonСalculate);
+            this.Controls.Add(this.q);
+            this.Controls.Add(this.w);
+            this.Controls.Add(this.textBoxQ);
+            this.Controls.Add(this.textBoxW);
+            this.Controls.Add(this.g);
+            this.Controls.Add(this.e);
+            this.Controls.Add(this.textBoxE);
+            this.Controls.Add(this.textBoxG);
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Laba_1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxG;
+        private System.Windows.Forms.TextBox textBoxE;
+        private System.Windows.Forms.Label e;
+        private System.Windows.Forms.Label g;
+        private System.Windows.Forms.TextBox textBoxW;
+        private System.Windows.Forms.TextBox textBoxQ;
+        private System.Windows.Forms.Label w;
+        private System.Windows.Forms.Label q;
+        private System.Windows.Forms.Button buttonСalculate;
+        private System.Windows.Forms.Label Answer;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label x;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ laba12/Laba1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = string.Format("Координаты: {0}, {1}", e.X, e.Y);
+            textBoxE.Text = (e.X + e.Y).ToString();
+
+            textBoxW.Text = (e.X + e.Y).ToString();
+
+
+        }
+
+        private void buttonСalculate_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                double g = Convert.ToDouble(textBoxG.Text);
+
+                double q = Convert.ToDouble(textBoxQ.Text);
+
+                double x = Convert.ToDouble(textBoxX.Text);
+
+                double answer = Convert.ToDouble(textBoxW.Text) + (Math.Tan(g * q)/Math.Log10(20*x)) 
+                    - (Convert.ToDouble(textBoxE.Text)/ Convert.ToDouble(textBoxW.Text)) 
+                    + Math.Abs(Math.Sin(Convert.ToDouble(textBoxE.Text) 
+                    + Math.Sqrt(Math.Abs(Convert.ToDouble(textBoxW.Text) - Convert.ToDouble(textBoxE.Text)))));
+
+                this.Text = answer.ToString();
+            }
+            catch (Exception ) {
+                this.Text = "ERROR";
+            }
+        }
+
+
+    }
+}
