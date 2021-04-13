0d40e266f1dae82a4cc49fda9611a1e3c1b0f71c
Inozemcev-INS-19 <71772161+Inozemcev-INS-19@users.noreply.github.com>
2021-04-13 21:53:26 +0300

    Add files via upload

---
+++ laba15/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace laba_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFont = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.labelN = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.buttonAnswer = new System.Windows.Forms.Button();
+            this.textBoxAnswer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.labelX = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBoxOpenFile = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonFont
+            // 
+            this.buttonFont.Location = new System.Drawing.Point(13, 13);
+            this.buttonFont.Name = "buttonFont";
+            this.buttonFont.Size = new System.Drawing.Size(84, 23);
+            this.buttonFont.TabIndex = 0;
+            this.buttonFont.Text = "ChangeFont";
+            this.buttonFont.UseVisualStyleBackColor = true;
+            this.buttonFont.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Location = new System.Drawing.Point(119, 13);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(84, 23);
+            this.buttonColor.TabIndex = 1;
+            this.buttonColor.Text = "ChangeColor";
+            this.buttonColor.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(371, 12);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(84, 23);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "FileSave";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(488, 13);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(84, 23);
+            this.buttonOpen.TabIndex = 4;
+            this.buttonOpen.Text = "FileOpen";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelN.Location = new System.Drawing.Point(4, 64);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(24, 24);
+            this.labelN.TabIndex = 5;
+            this.labelN.Text = "N";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::laba_4.Properties.Resources.Снимок1;
+            this.pictureBox1.Location = new System.Drawing.Point(371, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(273, 50);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(34, 68);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(100, 20);
+            this.textBoxN.TabIndex = 7;
+            this.textBoxN.Text = "0";
+            // 
+            // buttonAnswer
+            // 
+            this.buttonAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAnswer.Location = new System.Drawing.Point(12, 223);
+            this.buttonAnswer.Name = "buttonAnswer";
+            this.buttonAnswer.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.buttonAnswer.Size = new System.Drawing.Size(75, 46);
+            this.buttonAnswer.TabIndex = 8;
+            this.buttonAnswer.Text = "Catch answer";
+            this.buttonAnswer.UseVisualStyleBackColor = true;
+            this.buttonAnswer.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBoxAnswer
+            // 
+            this.textBoxAnswer.Location = new System.Drawing.Point(110, 249);
+            this.textBoxAnswer.Name = "textBoxAnswer";
+            this.textBoxAnswer.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAnswer.TabIndex = 9;
+            this.textBoxAnswer.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(107, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 18);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "answer";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(34, 139);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 11;
+            this.textBoxX.Text = "0";
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelX.Location = new System.Drawing.Point(4, 139);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(21, 20);
+            this.labelX.TabIndex = 12;
+            this.labelX.Text = "X";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBoxOpenFile
+            // 
+            this.richTextBoxOpenFile.Location = new System.Drawing.Point(371, 175);
+            this.richTextBoxOpenFile.Name = "richTextBoxOpenFile";
+            this.richTextBoxOpenFile.Size = new System.Drawing.Size(273, 138);
+            this.richTextBoxOpenFile.TabIndex = 13;
+            this.richTextBoxOpenFile.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(367, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Open File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 325);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBoxOpenFile);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAnswer);
+            this.Controls.Add(this.buttonAnswer);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelN);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonColor);
+            this.Controls.Add(this.buttonFont);
+            this.Location = new System.Drawing.Point(50, 50);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFont;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Button buttonAnswer;
+        private System.Windows.Forms.TextBox textBoxAnswer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBoxOpenFile;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ laba15/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace laba_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            buttonColor.Click += buttonColor_Click;
+            colorDialog1.FullOpen = true;
+            colorDialog1.Color = this.BackColor;
+
+            textBoxAnswer.Click += button1_Click;
+            fontDialog1.ShowColor = true;
+
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            textBoxAnswer.Font = fontDialog1.Font;
+            textBoxAnswer.ForeColor = fontDialog1.Color;
+
+            textBoxN.Font = fontDialog1.Font;
+            textBoxN.ForeColor = fontDialog1.Color;
+
+            textBoxX.Font = fontDialog1.Font;
+            textBoxX.ForeColor = fontDialog1.Color;            
+        }
+
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.Cancel) return;
+            this.BackColor = colorDialog1.Color;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int N = Convert.ToInt32(textBoxN.Text);
+            int c = 0;
+            double answer = 0;
+            double x = Convert.ToDouble(textBoxX.Text);
+            for (int i = 1; i <= N; i++) {
+                c += i;
+                
+                if (i % 2 != 0)
+                    answer -= (Math.Pow(x, c)) / (c * (c + 1) * (c + 2));
+                else answer += (Math.Pow(x, c)) / (c * (c + 1) * (c + 2));
+                
+                c = i;
+            }
+            textBoxAnswer.Text = answer.ToString("0.00");
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            File.WriteAllText(filename, textBoxAnswer.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+
+            string filename = openFileDialog1.FileName;
+
+            string Answer = File.ReadAllText(filename);
+
+            richTextBoxOpenFile.Text = Answer;
+            MessageBox.Show("Open");
+
+
+           
+
+        }
+    }
+}
