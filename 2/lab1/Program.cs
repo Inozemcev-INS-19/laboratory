b5b5ce663c005eb3f0ea1b32b11bf9ed0c9f3aa7
Inozemcev-INS-19 <71772161+Inozemcev-INS-19@users.noreply.github.com>
2023-04-02 14:03:43 +0300

    Add files via upload

---
+++ 2/lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<Action<string, string>, bool> func = GetTrue; 
+            Action<string, string> action = Draw; //инициализация делегата
+            func.Invoke(action); //вызов делегата
+            func = GetFalse; // инициализация делегата
+            func.Invoke(action); //вызов делегата
+
+        }
+
+        static void Draw(string x, string y) 
+        {
+            Console.WriteLine($"{x} and {y}");
+
+        }
+        static bool GetTrue(Action<string, string> draw) 
+        {
+            draw.Invoke("word", "word2");
+            return true;
+        }
+        static bool GetFalse(Action<string, string> draw)
+        {
+            draw.Invoke("elemination", "word4");
+            return false;
+        }
+
+
+    }
+}
